bda941eae31be779fb3ac8349d29279128e7e167
DmitriqPanteleev <pantele3vd@gmail.com>
2022-11-15 14:36:49 +0400

    Initial Commit

---
+++ AliasForIT/AliasForITApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliasForITApp.swift
+//  AliasForIT
+//
+//  Created by Дмитрий Пантелеев on 15.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct AliasForITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
