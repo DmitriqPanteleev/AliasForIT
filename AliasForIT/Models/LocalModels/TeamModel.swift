9db423f39c526af48c8599aa30615c46dcf9ca6a
DmitriqPanteleev <pantele3vd@gmail.com>
2022-12-04 18:41:55 +0400

    GameViewModel is ready

--- AliasForIT/Models/LocalModels/TeamModel.swift
+++ AliasForIT/Models/LocalModels/TeamModel.swift
@@ -8,12 +8,37 @@
 import Foundation
 
 struct TeamModel: Equatable {
+    let id: Int
     let name: String
     let image: String
-    let score: Int
+    var score: Int
+    
+    init(name: String, image: String) {
+        self.id = UUID().hashValue
+        self.name = name
+        self.image = image
+        self.score = 0
+    }
+    
+    init(name: String, image: String, score: Int) {
+        self.id = UUID().hashValue
+        self.name = name
+        self.image = image
+        self.score = score
+    }
     
     var imageSource: URL? {
         guard let source = URL(string: image) else { return nil }
         return source
+    }
+    
+    static func defaultTeam1() -> TeamModel {
+        TeamModel(name: "Константы",
+                  image: "https://yt3.ggpht.com/ytc/AMLnZu9CTrwR1EFNZuEqb9TNFEAqBNMyFz1ylQNBNRVgwQ=s900-c-k-c0x00ffffff-no-rj")
+    }
+    
+    static func defaultTeam2() -> TeamModel {
+        TeamModel(name: "Умер в конце драйва",
+                  image: "https://i.pinimg.com/originals/56/6c/aa/566caa613d4c3ea046f1db3b30487e41.jpg")
     }
 }
---
+++ AliasForIT/Modules/GameView/GameViewModel.swift
@@ -0,0 +1,81 @@
+//
+//  GameViewModel.swift
+//  AliasForIT
+//
+//  Created by Дмитрий Пантелеев on 03.12.2022.
+//
+
+import Foundation
+import Combine
+
+final class GameViewModel: ObservableObject {
+    
+    let teams: [TeamModel]
+    
+    var currentTeamIndex: Int
+    let input: Input
+    @Published var output: Output
+    
+    
+    private var cancellable = Set<AnyCancellable>()
+    
+    init(teams: [TeamModel]) {
+        
+        self.currentTeamIndex = 0
+        self.teams = teams
+        
+        self.input = Input()
+        self.output = Output()
+        
+        setupBindings()
+    }
+    
+    deinit {
+        #if DEBUG
+        print("\(self) DEINITED!!!")
+        #endif
+        
+        cancellable.forEach { $0.cancel() }
+        cancellable.removeAll()
+    }
+    
+    func setupBindings() {
+        
+        output.teams = self.teams
+        output.currentTeam = self.teams[currentTeamIndex]
+        
+        bindRoundFinish()
+    }
+    
+    // TODO: trigger this func in RoundViewModel (on timer ends)
+    func bindRoundFinish() {
+        input.onRoundFinish
+            .sink { [weak self] newScore in
+                
+                guard let self = self else { return }
+                
+                self.output.currentTeam?.score += newScore
+                
+                self.output.pointsLeft[self.currentTeamIndex] -= self.output.currentTeam!.score
+                
+                self.currentTeamIndex += self.currentTeamIndex == self.teams.count - 1 ? 1 : -self.teams.count + 1
+                self.output.currentTeam = self.teams[self.currentTeamIndex]
+                
+                self.input.onAppear.send()
+            }
+            .store(in: &cancellable)
+    }
+    
+    
+    struct Input {
+        
+        let onAppear = PassthroughSubject<Void, Never>()
+        let onRoundFinish = PassthroughSubject<Int, Never>()
+    }
+    
+    struct Output {
+        var teams: [TeamModel] = []
+        var currentTeam: TeamModel? = nil
+        var pointsLeft: [Int] = []
+    }
+}
